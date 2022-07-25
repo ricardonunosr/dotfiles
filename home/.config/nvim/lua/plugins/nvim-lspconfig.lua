08414175126d2d004beb2a96b2009c748e4aa516
ricardonunosr <ricardonunosr@gmail.com>
2022-07-25 21:52:02 +0100

    add efm language server for formatting

--- home/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ home/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -116,3 +116,20 @@ for _, lsp in ipairs(servers) do
 		}
 	}
 end
+
+require "lspconfig".efm.setup {
+	on_attach = on_attach,
+	flags = {
+		debounce_text_changes = 150,
+	},
+	init_options = { documentFormatting = true },
+	filetypes = { "python" },
+	settings = {
+		rootMarkers = { ".git/" },
+		languages = {
+			python = {
+				{ formatCommand = "black --quiet -", formatStdin = true }
+			}
+		}
+	}
+}
